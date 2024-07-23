21a36d55e0a3e256449daf54ad60b65e0a9932f9
MedelDiego-1317 <diegomedelg@gmail.com>
2024-07-23 17:01:28 -0600

    Version final del codigo que se encarga de leer un archivo ASC, de tal manera que separa todos sus datos en archivos CSV.

--- wcad_lector_vDiego.py
+++ wcad_lector_vDiego.py
@@ -1,3 +1,7 @@
+# Instituto nacional de cancerologia
+# Hecho por Diego Medel
+# 23/07/24
+
 
 import re
 import numpy as np
@@ -26,19 +30,24 @@ with open("C:/Users/diego/OneDrive/Escritorio/Universidad/python/Proyectos INCAN
     for line in data_file:
         data=re.split('[<+>]', line)
 
-        if line.startswith("%DPTH"):
+        if line.startswith("%DPTH"): # Lo que quiero es obtener el numero de profundidades que se consideran
             profundidades.append(data)
 
 profundidades_lista = []
 
 for i in profundidades:
-    profundidades_lista.extend(i)
+    profundidades_lista.extend(i) # Lo que hace es aplanar una lista de listas, para asi solo obtener una lista de todos los titulos de profundidades
 
 
+#----------------------------------------------------------------------------------------------------------#
+
+# Necesito crear listas vacias, para despues rellenarlas con datos. Seran listas de listas
 listas_rellenables = [[] for i in profundidades_lista]
 ejesy = [[] for i in profundidades_lista]
 ejesx = [[] for i in profundidades_lista]
 
+
+#----------------------------------------------------------------------------------------------------------#
 
 
 with open("C:/Users/diego/OneDrive/Escritorio/Universidad/python/Proyectos INCAN/X15_Perfiles.ASC",'r') as data_file:
@@ -48,29 +57,33 @@ with open("C:/Users/diego/OneDrive/Escritorio/Universidad/python/Proyectos INCAN
     for line in data_file:
         data=re.split('[<+>]', line)
 
-        if line.startswith("<"):
+        if line.startswith("<"): # Si empieza con el simbolo < entonces que continue
             inside_tag = True
-            if n < len(listas_rellenables):
-                listas_rellenables[n].extend([data])  # Asegúrate de que n esté dentro del rango
+            if n < len(listas_rellenables): # Cuida que no se pase del rango
+                listas_rellenables[n].append(data)  # Va a generar una lista con indice n, donde metere listas de valores, hasta que se lea ENOM, una vez que lo haya leido, lo que hara es sumar un 1 a esa n, por lo que creara una n = 2.
+
         elif line.startswith("$ENOM"):
             inside_tag = False
             n += 1
 
 
 
-for i, lista in enumerate(listas_rellenables):
-    for sublista in lista:
-        if len(sublista) >= 4:
-            ejesx[i].append(sublista[1])
-            ejesy[i].append(sublista[4])
+for i, lista in enumerate(listas_rellenables): # El i indice el indice y lista los elementos dentro de listas rellenables, es decir, puedo estar en la lista 1, compuesta de varias listas
+    for sublista in lista: # Sublista es un elemento de la variable lista
+        end = len(sublista) # Determinamos el tamaño de esta sublista
+        if end > 5:  
+            ejeydata = re.sub("'", "", sublista[end-2]) # Aqui hay algo muy importante, sin embargo no identifico que es, solamente puedo decir que sin este comando la longitud de las listas cuando el valor es positivo incrementa
+            ejesy[i].append(ejeydata) # Almacena los datos
+            ejesx[i].append(sublista[end-5])
 
 
-
-
-for i, (ejes_x, ejes_y) in enumerate(zip(ejesx,ejesy)):
-    df = pd.DataFrame({
-        "x": ejes_x,
-        "y": ejes_y
-    })
-    filename = f"lista_{i+1}_data.csv"
-    df.to_csv(filename,index=False)
+for i, (ejes_x, ejes_y) in enumerate(zip(ejesx, ejesy)): # Algo complejo pero zip conjunta dos arreglos en una, es decir, genera una tupla que puede ir siendo indexada, pero cada arreglo tiene su variable de elementos propia
+    if len(ejes_x) == len(ejes_y):  # Cuida que la longitud de los elementos sea la misma, para poder generar un data frame
+        df = pd.DataFrame({
+            "x": ejes_x,
+            "y": ejes_y
+        })
+        filename = f"lista_{i+1}_data.csv"
+        df.to_csv(filename, index=False)
+    else:
+        print(f"Error: Longitudes desiguales en lista {i+1}")
