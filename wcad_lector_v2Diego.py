ebf8a1c341b16a18fc6ea33b9b1c92b42c80a508
MedelDiego-1317 <diegomedelg@gmail.com>
2024-07-31 19:42:33 -0600

    Esta version del codigo permite exportar los perfiles del acelerador lineal considerando la profundidad y el tamaño de campo, lo obtenido lo guarda en un archivo csv

--- wcad_lector_v2Diego.py
+++ wcad_lector_v2Diego.py
@@ -13,30 +13,90 @@ import pandas as pd
 
 inside_tag = False
 profundidades = []
+tc = []
+tipo =[]
+ssd=[]
+
+
 output = []
 ejey = []
 ejex = []
 ejey1 = []
 ejex1 = []
 titulos = []
+prof_medida=""
+ssd=[]
+tamCampo=""
+
 n=0
 curva_text = "none"
 scan = "empty"
-tc= "none"
+tc= []
 df2 = pd.DataFrame()
 
+#%DATE 30-00-24
+#%DETY CHA
+#%BMTY PHO
+#%TYPE OPP
+#%AXIS X
+#%PNTS 115
+#%STEP 010
+#%SSD  1000
+#%FLSZ 030*030
+#%DPTH 050
 
 with open("C:/Users/diego/OneDrive/Escritorio/Universidad/python/Proyectos INCAN/X15_Perfiles.ASC",'r') as data_file:
     for line in data_file:
         data=re.split('[<+>]', line)
 
-        if line.startswith("%FLSZ"): # Lo que quiero es obtener el numero de profundidades que se consideran
+        if line.startswith("%DPTH"): # Lo que quiero es obtener el numero de profundidades que se consideran
             profundidades.append(data)
+            #pre=line.strip().split(' ')[1]
+            #prof_medida=str(pre+str(n))
+
+        if line.startswith("%FLSZ"): # Lo que quiero es obtener el numero de profundidades que se consideran
+            tc.append(data)
+
+        if line.startswith("%SSD"): # Lo que quiero es obtener el numero de profundidades que se consideran
+            ssd.append(data)
 
 profundidades_lista = []
+ssd_lista = []
+tc_lista =[]
 
 for i in profundidades:
     profundidades_lista.extend(i) # Lo que hace es aplanar una lista de listas, para asi solo obtener una lista de todos los titulos de profundidades
+
+for i in ssd:
+    ssd_lista.extend(i)
+
+for i in tc:
+    tc_lista.extend(i)
+
+
+tamaño_campo = []
+
+for i in tc_lista:
+    nc = i.split()
+    campo = nc[1]
+    tamaño_campo.append(campo)
+
+tamaño_corregido = []
+
+for i in tamaño_campo:
+    tcc = i.split("*")
+    cor = tcc[1]
+    tamaño_corregido.append(cor)
+
+
+prof = []
+
+for i in profundidades_lista:
+    npp = i.split()
+    p = npp[1]
+    prof.append(p)
+
+
 
 
 #----------------------------------------------------------------------------------------------------------#
@@ -50,8 +110,8 @@ ejesx = [[] for i in profundidades_lista]
 #----------------------------------------------------------------------------------------------------------#
 
 
-with open("C:/Users/diego/OneDrive/Escritorio/Universidad/python/Proyectos INCAN/X15_PDD.ASC",'r') as data_file:
-    
+with open("C:/Users/diego/OneDrive/Escritorio/Universidad/python/Proyectos INCAN/X15_Perfiles.ASC",'r') as data_file:
+
     n = 0
 
     for line in data_file:
@@ -72,10 +132,10 @@ with open("C:/Users/diego/OneDrive/Escritorio/Universidad/python/Proyectos INCAN
 for i, lista in enumerate(listas_rellenables): # El i indice el indice y lista los elementos dentro de listas rellenables, es decir, puedo estar en la lista 1, compuesta de varias listas
     for sublista in lista: # Sublista es un elemento de la variable lista
         end = len(sublista) # Determinamos el tamaño de esta sublista
-        if end > 5:  
+        if end > 5:
             ejeydata = re.sub("'", "", sublista[end-2]) # Aqui hay algo muy importante, sin embargo no identifico que es, solamente puedo decir que sin este comando la longitud de las listas cuando el valor es positivo incrementa
             ejesy[i].append(ejeydata) # Almacena los datos
-            ejesx[i].append(sublista[end-3])
+            ejesx[i].append(sublista[end-5])
 
 #----------------------------------------------------------------------------------------------------------#
 
@@ -86,8 +146,24 @@ for i, (ejes_x, ejes_y) in enumerate(zip(ejesx, ejesy)): # Algo complejo pero zi
             "x": ejes_x,
             "y": ejes_y
         })
-        filename = f"lista_{i+1}_data.csv"
-        df.to_csv(filename, index=False)
+
+        x = pd.to_numeric(df['x'])
+        y = pd.to_numeric(df['y'])
+
+        f = interpolate.interp1d(x,y)
+        maximoX= pd.to_numeric(df['x'].max())
+
+        xMedidoInterpolado = np.arange(-maximoX,maximoX,1)
+        yMedidoInterpolado = f(xMedidoInterpolado)
+        df3 = pd.DataFrame()
+
+        df3.insert(0, 'Xres', xMedidoInterpolado, False)
+        df3.insert(1, 'Yres', yMedidoInterpolado, False)
+
+        filename2 =f"tamaño de campo_{tamaño_corregido[i]}x{tamaño_corregido[i]}_profundidad_{prof[i]}.csv"
+        #filename = f"lista_{i+1}_data.csv"
+        df3.to_csv(filename2, index=False)
+        #df.to_csv(filename, index=False)
     else:
         print(f"Error: Longitudes desiguales en lista {i+1}")
 
